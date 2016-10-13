d0701e67de876467d29d25fff5d5f1d5c02c60ee
mangu3804 <mangu3804@7LKL6Y1.Student.UGDSB.ED>
2016-10-13 13:58:24 -0400

    2

--- src/cartester/CarTester.java
+++ src/cartester/CarTester.java
@@ -19,23 +19,47 @@ public class CarTester {
         Scanner input = new Scanner(System.in);
         
         for (int i = 0; i < 3; i++) {
-            System.out.println("--------------------------");
-            System.out.println("Car " + (i + 1));
-            System.out.println("--------------------------");
-            System.out.println("Enter the make of the car:"); //Necessary
-            carMake = input.nextLine();
-            System.out.println("Enter the model of the car:"); //Necessary
-            carModel = input.nextLine();
-            System.out.println("Enter the colour of the car:"); //Necessary
-            carColour = input.nextLine();
-            System.out.println("Enter the year of the car:"); //Necessary
-            carYear = input.nextInt();
-            System.out.println("Enter the age of the car:");
-            carAge = input.nextInt();
-            System.out.println("Enter the price of the car");
-            carPrice = input.nextInt();
-            cars[i] = new Car(carMake, carModel, carColour, carYear, carAge,
-                    carPrice);
+            if (true) {
+                System.out.println("--------------------------");
+                System.out.println("Car " + (i + 1));
+                System.out.println("--------------------------");
+                System.out.print("Enter the make of the car: "); //Necessary
+                carMake = input.nextLine();
+                System.out.print("Enter the model of the car: "); //Necessary
+                carModel = input.nextLine();
+                System.out.print("Enter the colour of the car: "); //Necessary
+                carColour = input.nextLine();
+                System.out.print("Enter the year of the car: "); //Necessary
+                //carYear = input.nextInt();
+                    carYear = 0;
+                try {
+                    carYear = Integer.parseInt(input.nextLine());
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+                System.out.print("Enter the age of the car: ");
+                //carAge = input.nextInt();
+                    carAge = 0;
+                try {
+                    carAge = Integer.parseInt(input.nextLine());
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+                System.out.print("Enter the price of the car: ");
+                //carPrice = input.nextInt();
+                carPrice = 0;
+                try {                    
+                    carPrice = Integer.parseInt(input.nextLine());
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+                //input.nextLine();  // Consume newline left-over
+                cars[i] = new Car(carMake, carModel, carColour, carYear, carAge,
+                        carPrice);
+            } else {
+                
+            }
+            
         }
         
         //Car car1 = new Car(carMake, carModel, carColour, carYear, carAge, carPrice);
